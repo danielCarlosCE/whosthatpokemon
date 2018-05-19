baf0e5f15c5e218871a566c7c8386b59c3e3fea2
danielcarlosce <danielcarlossmd@gmail.com>
2018-05-19 14:03:21 -0300

    Add tabbar and navigation controller
    - the pokemonslistViewController.viewModel doesn’t need to know about this change, only the Coordinator

--- whosthatpokemon/Coordinators/AppCoordinator.swift
+++ whosthatpokemon/Coordinators/AppCoordinator.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Rswift
 
 protocol Coordinator {
     var viewController: UIViewController { get }
@@ -9,11 +8,31 @@ class AppCoordinator: Coordinator {
     let viewController: UIViewController
 
     init() {
+        let tabbar = UITabBarController()
+        viewController = tabbar
+
+        let listNavigation = pokemonsListNavigation()
+
+        let favorites = UIViewController()
+        favorites.tabBarItem =
+            UITabBarItem(title: "My pokemons", image: R.image.pokeball(), selectedImage: nil)
+
+        tabbar.viewControllers = [listNavigation, favorites]
+
+    }
+
+    func pokemonsListNavigation() -> UINavigationController {
         guard let pokemonslistViewController = R.storyboard.main.pokemonsList() else {
             fatalError("Rswift wasn't able to find pokemonsList at \(#function)")
         }
 
         pokemonslistViewController.viewModel = PokemonsListViewModel(service: PokemonService())
-        viewController = pokemonslistViewController
+
+        let listNavigation = UINavigationController(rootViewController: pokemonslistViewController)
+        listNavigation.tabBarItem =
+            UITabBarItem(title: "Catcha them all", image: R.image.backpack(), selectedImage: nil)
+        listNavigation.setNavigationBarHidden(true, animated: false)
+
+        return listNavigation
     }
 }
